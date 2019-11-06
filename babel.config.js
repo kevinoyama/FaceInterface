caf8fafe102c36d0ef62675a0188e061af6df48a
unknown <kevinken@souescoteiro.org.br>
2019-11-06 18:36:47 -0300

    Setting up the application

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    presets: [
+        "@babel/preset-env", //Alterar as funcionalidades nova do JS que o navegador não entende
+        "@babel/preset-react" //Alterar as funcionalidades nova do React que o navegador não entende
+    ],
+}
