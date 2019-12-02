0ef8641886523b1a561a0d078b0337b4cf3b6e33
unknown <kevinken@souescoteiro.org.br>
2019-12-01 21:53:45 -0300

    deletion of unecessary file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './App.css';
-import TechList from './components/TechList';
 import Header from './components/Header';
 import PostList from './components/PostList';
 
 function App(){
     return (
         <>
-        <Header/>
-        <PostList/>
+            <Header/>
+            <PostList/>
         </>
     )
 
